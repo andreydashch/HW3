59c8a0bceda5e85c79219dc1b40ba780963fb75b
andreydashch <andreydashch@gmail.com>
2019-11-24 18:55:15 +0200

    Refactor tests

--- TDD2Video/TDD2Video/src/test/java/ua/training/tdd/TestArabicToRomanNumber.java
+++ TDD2Video/TDD2Video/src/test/java/ua/training/tdd/TestArabicToRomanNumber.java
@@ -5,18 +5,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestArabicToRomanNumber {
+    String romanNumber;
 
     @Test
     public void Convert_0_To_EmptyString() {
-        String romanNumber;
         romanNumber = ArabicToRomanNumber.convert(0);
-        Assert.assertEquals(romanNumber, "");
+        Assert.assertEquals("", romanNumber);
     }
 
     @Test
     public void Convert_1_To_I() {
-        String romanNumber;
         romanNumber = ArabicToRomanNumber.convert(1);
-        Assert.assertEquals(romanNumber, "I");
+        Assert.assertEquals("I", romanNumber);
     }
+
+
 }
