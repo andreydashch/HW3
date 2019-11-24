37f2d2bf8a807f0a8069e184b4bf30d3875eb5e8
andreydashch <andreydashch@gmail.com>
2019-11-24 18:29:50 +0200

    Proper create TDD2Video project

---
+++ TDD2Video/TDD2Video/src/main/java/ua/training/tdd/ArabicToRomanNumber.java
@@ -0,0 +1,18 @@
+/*
+ * @(#) ArabicToRomanNumber.java
+ *
+ * This software can be used by anyone
+ * with no limit. But developer do not
+ * granite its proper working.
+ */
+
+
+package ua.training.tdd;
+
+/**
+ * Converter from arabic numbers to
+ * roman.
+ */
+public class ArabicToRomanNumber {
+
+}
---
+++ TDD2Video/TDD2Video/src/test/java/ua/training/tdd/TestArabicToRomanNumber.java
@@ -0,0 +1,8 @@
+package ua.training.tdd;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestArabicToRomanNumber {
+
+}
