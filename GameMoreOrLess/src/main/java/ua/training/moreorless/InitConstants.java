f5ec55c1b6aedcb87be0c60c175531cfe31a7570
andreydashch <andreydashch@gmail.com>
2019-11-25 09:31:34 +0200

    Add InitConstants

---
+++ GameMoreOrLess/src/main/java/ua/training/moreorless/InitConstants.java
@@ -0,0 +1,6 @@
+package ua.training.moreorless;
+
+public class InitConstants {
+    private static final int MAX_RANDOM_TARGET = 100;
+    private static final int MIN_RANDOM_TARGET = 0; // must be bigger than 0
+}
