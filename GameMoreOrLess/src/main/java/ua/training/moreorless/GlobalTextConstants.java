ba3c1c4d58e09a8e36a73fca0b7829574294e51f
andreydashch <andreydashch@gmail.com>
2019-11-25 09:17:16 +0200

    Add GlobalTextConstants

---
+++ GameMoreOrLess/src/main/java/ua/training/moreorless/GlobalTextConstants.java
@@ -0,0 +1,10 @@
+package ua.training.moreorless;
+
+public class GlobalTextConstants {
+    public static final String RULES =
+            "Player must guess integer number\n"+
+            "from range, it will converse while\n"+
+            "payer will be coming closer.\n\n";
+    public static final String CONGRATULATION =
+            "It was guessed number\nCongratulations!!!";
+}
